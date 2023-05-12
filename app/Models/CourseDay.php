6a1839344b66b88fcc6fb297bbea25749977ccc3
hala m <hala@futurecode-co.com>
2023-05-12 18:41:51 +0300

    models name

--- app/Models/CourseDay.php
+++ app/Models/CourseDay.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course_Day extends Model
+class CourseDay extends Model
 {
     use HasFactory;
     protected $fillable=[
--- app/Models/GuestPlacement.php
+++ app/Models/GuestPlacement.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GuestQuestionList extends Model
+class GuestPlacement extends Model
 {
     use HasFactory;
     protected $fillable=[
