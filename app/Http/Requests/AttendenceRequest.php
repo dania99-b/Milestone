8f36c4e28b1fc217483023d767ec6b92c576c728
dania99-b <dania1999ta@gmail.com>
2023-05-26 17:40:21 +0300

    scan Barcode2

--- app/Http/Requests/AttendenceRequest.php
+++ app/Http/Requests/AttendenceRequest.php
@@ -24,7 +24,7 @@ class AttendenceRequest extends FormRequest
     public function rules()
     {
         return [
-        'course_id'=>'required',
+       
         'qr_code'=>'string'
         ];
     }
